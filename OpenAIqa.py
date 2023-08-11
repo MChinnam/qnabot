c903f0e3bc3a5ab3acf1b3e3b86ebbc1f700c4b0
MChinnam <43243824+MChinnam@users.noreply.github.com>
2023-08-11 13:28:34 +0530

    Add files via upload

---
+++ OpenAIqa.py
@@ -0,0 +1,133 @@
+import os
+import json
+import copy
+import logging
+import sys
+os.environ["HNSWLIB_NO_NATIVE"] = '1'
+from langchain.document_loaders import url_selenium, WebBaseLoader
+
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+
+
+
+
+from __init__ import OPENAI_API_KEY, template,url
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s :[%(levelname)s]: %(message)s")
+logging.StreamHandler(sys.stdout)
+
+
+
+# import nltk
+# import ssl
+
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+
+#nltk.download()
+
+class OpenAQuestionAnswering:
+    """
+    OpenAI Question Answering
+    """
+    OPENAI_API_KEY = None
+    url=[]
+    all_documents = []
+    embeddings = None
+    chain = None
+
+    def __init__(self, urls = []):
+        """
+        setting OpenAI environment variables
+        :param urls:
+        """
+        try:
+            self.OPENAI_API_KEY = OPENAI_API_KEY
+            os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+            self.urls = urls
+            self.embeddings = OpenAIEmbeddings()
+            self.prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+            logging.info("Successfully initialized OpenAI Key Environment variable")
+        except Exception as ex:
+            logging.error(f"Error while setting OpenAI Key Environment variable: {ex}")
+        return
+
+    def load_urls(self):
+        try:
+            if len(self.urls) == 0:
+                logging.info("No urls available")
+                return
+            case_studies_exists = False
+            case_studies_url = "http://fissionlabs.com/case-studies"
+            if case_studies_url in self.urls:
+                self.urls.remove(case_studies_url)
+                case_studies_exists = True
+            if len(self.urls)>0:
+                loader = url_selenium.SeleniumURLLoader(urls=self.urls)
+                self.all_documents.extend(copy.deepcopy(loader.load()))
+            if case_studies_exists:
+                loader = WebBaseLoader(urls=self.urls)
+                self.all_documents.extend(copy.deepcopy(loader.load()))
+                self.urls.append(case_studies_url)
+            logging.info(f"Successfully data: {len(self.all_documents)}")
+        except Exception as ex:
+            logging.error(f"Error while loading data: {ex}")
+        return
+
+
+
+    def load_data(self):
+        try:
+            self.load_urls()
+            if len(self.all_documents)==0:
+                logging.info(f"we don't have any documents to connect to openai")
+                return
+            db = Chroma.from_documents(self.all_documents,self.embeddings)
+            chain_type_kwargs = {"prompt": self.prompt}
+            self.chain = RetrievalQA.from_chain_type(
+                llm=ChatOpenAI(temperature=0),
+                chain_type="stuff",
+                retriever=db.as_retriever(),
+                chain_type_kwargs=chain_type_kwargs,
+            )
+            logging.info("Successfully loaded data to chromadb and connected to langchain")
+        except Exception as ex:
+            logging.error(f"Error while loading documents to chromadb or connecting to OpenAI: {ex}")
+        return
+
+    def query_data(self, query):
+        defauult_output={}
+        try:
+            if self.urls==0:
+                return "No urls exists"
+            if self.all_documents==0:
+                return "Not able to load document using langchain Selenium loader"
+            if self.chain is None:
+                return "Not able to connect to OpenAI or failed to create chromdb"
+            response = self.chain.run(query)
+            output = json.loads(response)
+            if output.get('source_url',"N/A")=="N/A":
+                return "No answer found"
+            #return output["Answer"]
+            return output
+        except Exception as ex:
+            logging.error(f"Error while querying the data: {ex}")
+        return "Error while query OpenAI"
+
+
+# if __name__=='__main__':
+#     # for chromadb installation export HNSWLIB_NO_NATIVE=1
+#     openai_question_answer = OpenAQuestionAnswering(["https://www.fissionlabs.com/about-us"])
+#     openai_question_answer.load_data()
+#     print(openai_question_answer.query_data("Kishore Poreddy"))
+
+
---
+++ QnA_Maker_Rest_api.py
@@ -0,0 +1,197 @@
+import copy
+from fastapi import FastAPI
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
+import uvicorn
+import logging
+import sys
+from pydantic import BaseModel
+from OpenAIqa import OpenAQuestionAnswering
+OpenAIqa=OpenAQuestionAnswering()
+import os
+os.environ["HNSWLIB_NO_NATIVE"] = '1'
+logging.basicConfig(level=logging.INFO, format="%(asctime)s :[%(levelname)s]: %(message)s")
+logging.StreamHandler(sys.stdout)
+
+class Question(BaseModel):
+    question:str
+
+url=["http://fissionlabs.com/about-us",
+"http://fissionlabs.com/how-we-work",
+"http://fissionlabs.com/public-relations-csr",
+"http://fissionlabs.com/careers",
+"http://fissionlabs.com/contact-us",
+"http://fissionlabs.com/services/web-mobile-application-development-services",
+"http://fissionlabs.com/services/data-engineering-services",
+"http://fissionlabs.com/services/cloud-consulting-services",
+"http://fissionlabs.com/services/ai-ml-based-solutions",
+"http://fissionlabs.com/services/quality-assurance-services",
+"http://fissionlabs.com/services/salesforce-consulting-services",
+"http://fissionlabs.com/blog",
+"http://fissionlabs.com/case-studies",
+"https://www.fissionlabs.com/case-study/hospital-management-analytics-platform",
+"http://fissionlabs.com/case-study/ai-based-object-volume-estimator",
+"http://fissionlabs.com/case-study/multi-device-iot-communication-platform",
+"http://fissionlabs.com/case-study/realtime-health-records-monitoring-platform",
+"http://fissionlabs.com/case-study/fully-automated-managed-cloud-services-platform-for-healthcare-industry",
+"http://fissionlabs.com/case-study/advertising-sales-automation-workflow-platform"
+"https://www.fissionlabs.com/case-study/fleet-tracking-portal",
+"https://www.fissionlabs.com/case-study/pathology-radiology-e-learning-platform",
+"https://www.fissionlabs.com/case-study/enterprise-hrms-labour-analysis-platform",
+"http://fissionlabs.com/case-studies/integrated-campaign-management-platform",
+"http://fissionlabs.com/case-studies/workflow-management-platform",
+"http://fissionlabs.com/case-studies/iot-driven-fleet-management-platform",
+"http://fissionlabs.com/case-studies/life-science-and-regulatory-platform",
+"http://fissionlabs.com/case-studies/integrated-telehealth-platform",
+"http://fissionlabs.com/e-book-whitepapers",
+"http://fissionlabs.com/blog-posts/selecting-the-right-software-development-vendor-how-proof-of-concept-trials-can-help"]
+
+openai_question_answer = OpenAQuestionAnswering(urls=url)
+openai_question_answer.load_data()
+
+
+
+class QuestionAnswering:
+    """
+    Question Answering using Azure QnA Maker API
+    """
+    # You should load this from config file as it's just for testing we have directly update it
+    # It isn't a good practice when you scaling it as an API
+
+    # azure endpoint
+    endpoint = "https://sofibot-lang-service.cognitiveservices.azure.com/"
+
+    # azure credentials
+    credential = AzureKeyCredential("eb18002e72554e339d86d333618d78b8")
+
+    # project name
+    knowledge_base_project = "Fission-sales"
+
+    # It's the default value
+    deployment = "production"
+
+    # It would be updated once we connect to Azure QnAmaker API
+    client = None
+
+    # we are setting default confidence please feel free to fine-tune
+    confidence = None
+
+    def __init__(self, confidence=0.5):
+        """
+        Azure client
+        :param confidence:
+        """
+        try:
+            self.client = QuestionAnsweringClient(self.endpoint, self.credential)
+            self.confidence = confidence
+            logging.info("Successully connected to azure qa client")
+        except Exception as ex:
+            logging.error(f"Error while connecting to Azure: {ex}")
+
+    def extract_output(self,output):
+        """
+        Pre-processing extracted output from Azure QnAmaker API
+        :param output:
+        :return:
+        """
+        all_answers = []
+        default_json = {"answer": "N/A", "prompts": [], "confidence":0.0, "source":"N/A", "source_url":"N/A"}
+        try:
+            # if you want only the one with max confidence when there are multiple answers
+            # max_confidence = self.confidence
+            for answer in output.answers:
+                if answer.confidence>=self.confidence:
+                    #creating a copy so they aren't using same variable
+                    temp_json = copy.deepcopy(default_json)
+
+                    temp_json["answer"] = str(answer.answer).strip()
+                    temp_json["confidence"] = answer.confidence
+                    temp_json["source"] = answer.metadata.get("source","N/A")
+                    temp_json["source_url"] = answer.metadata.get("source_url", "N/A")
+                    if temp_json["source_url"]!="N/A":
+                        temp_json["source_url"] = temp_json["source_url"].replace("_com",".com").replace("@","/")
+                    # Checking if you have any prompts
+                    prompt_output = answer.dialog.prompts
+                    if len(prompt_output) > 0:
+                        temp_answer = ""
+                        for prompt in prompt_output:
+
+                            # appending points wrt header wrt streamlit to make it better on UI
+                            temp_answer = str(prompt.display_text).strip()
+                            if len(temp_answer) > 0:
+                                ## If no prompt output don't append it to the final answer
+                                temp_json["prompts"].append(temp_answer)
+                    all_answers.append(temp_json)
+            logging.info("Successfully extracted output\n")
+            return all_answers
+        except Exception as ex:
+            logging.error(f"Error while extracting output: {ex}")
+        return all_answers
+
+    def get_output(self, question):
+        """
+        Retrieving the output of a question
+        :param question:
+        :return:
+        """
+        try:
+            if self.client is None:
+                return "Error while connecting to Azure bot"
+            if type(question)!=str:
+                return "Enter a valid intput"
+            question = question.strip()
+            if len(question)==0:
+                return "enter a valid input"
+            output = self.client.get_answers(
+                question=question,
+                project_name=self.knowledge_base_project,
+                deployment_name=self.deployment
+            )
+            logging.info("Successfully retrieved output from azure environment")
+            return self.extract_output(output)[0]
+        except Exception as ex:
+            logging.error(f"Error getting accessing answer: {ex}")
+        return {}
+
+
+    def api_call(self,question):
+        resp=self.get_output(question)
+        if len(resp)!=0:
+            return resp
+        else: 
+            return openai_question_answer.query_data(question)
+
+
+
+app = FastAPI()
+qa_instance = QuestionAnswering()
+
+@app.get("/")
+async def check_service():
+    """
+    Checking if service is up and running
+    :return:
+    """
+    return {"status": 200, "message": "Service is up and running"}
+
+
+@app.post("/qa/")
+async def generate_response(question:Question):
+    prompt_items = []
+    output = qa_instance.api_call(question.question)
+    # answer = output["answers"][0].get("answer", "")
+    # if output["answers"][0]["questions"]:
+    #     prompts = output["answers"][0]["dialog"]["prompts"]
+    #     source = output["answers"][0]["metadata"].get("source")
+    # else:
+    #     prompts = []
+    #     source = None
+    # for each_prompt in prompts:
+    #     prompt_items.append(each_prompt.get('display_text'))
+    # return {"answer": answer, "prompts":prompt_items, "source":source}
+    
+    return output
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #print(qa_instance.api_call("kishore poreddy"))
