006737f338dc58662aa8f73b30995826ef0c92f2
MChinnam <43243824+MChinnam@users.noreply.github.com>
2023-08-11 15:24:18 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 loading configurations
 """
-OPENAI_API_KEY="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"
+OPENAI_API_KEY="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"
 
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
