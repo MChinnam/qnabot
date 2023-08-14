83875dc5cef5cb309ff4d9777fb9ec128c2700fd
MChinnam <43243824+MChinnam@users.noreply.github.com>
2023-08-14 16:11:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,34 +17,8 @@ class Question(BaseModel):
     question:str
 
 url=["http://fissionlabs.com/about-us",
-"http://fissionlabs.com/how-we-work",
-"http://fissionlabs.com/public-relations-csr",
-"http://fissionlabs.com/careers",
-"http://fissionlabs.com/contact-us",
-"http://fissionlabs.com/services/web-mobile-application-development-services",
-"http://fissionlabs.com/services/data-engineering-services",
-"http://fissionlabs.com/services/cloud-consulting-services",
-"http://fissionlabs.com/services/ai-ml-based-solutions",
-"http://fissionlabs.com/services/quality-assurance-services",
-"http://fissionlabs.com/services/salesforce-consulting-services",
-"http://fissionlabs.com/blog",
-"http://fissionlabs.com/case-studies",
-"https://www.fissionlabs.com/case-study/hospital-management-analytics-platform",
-"http://fissionlabs.com/case-study/ai-based-object-volume-estimator",
-"http://fissionlabs.com/case-study/multi-device-iot-communication-platform",
-"http://fissionlabs.com/case-study/realtime-health-records-monitoring-platform",
-"http://fissionlabs.com/case-study/fully-automated-managed-cloud-services-platform-for-healthcare-industry",
-"http://fissionlabs.com/case-study/advertising-sales-automation-workflow-platform"
-"https://www.fissionlabs.com/case-study/fleet-tracking-portal",
-"https://www.fissionlabs.com/case-study/pathology-radiology-e-learning-platform",
-"https://www.fissionlabs.com/case-study/enterprise-hrms-labour-analysis-platform",
-"http://fissionlabs.com/case-studies/integrated-campaign-management-platform",
-"http://fissionlabs.com/case-studies/workflow-management-platform",
-"http://fissionlabs.com/case-studies/iot-driven-fleet-management-platform",
-"http://fissionlabs.com/case-studies/life-science-and-regulatory-platform",
-"http://fissionlabs.com/case-studies/integrated-telehealth-platform",
-"http://fissionlabs.com/e-book-whitepapers",
-"http://fissionlabs.com/blog-posts/selecting-the-right-software-development-vendor-how-proof-of-concept-trials-can-help"]
+"http://fissionlabs.com/case-studies/iot-driven-fleet-management-platform"]
+
 
 openai_question_answer = OpenAQuestionAnswering(urls=url)
 openai_question_answer.load_data()
@@ -192,6 +166,6 @@ async def generate_response(question:Question):
     
     return output
 
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="0.0.0.0", port=8000)
-    #print(qa_instance.api_call("kishore poreddy"))
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+#     #print(qa_instance.api_call("kishore poreddy"))
